8f7e0fd5b27d97aa6a99bd9d4b76988f7d606a93
amsalimsema <salimsema@yahoo.com>
2023-01-03 11:53:14 +0300

    updateurl

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv)
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://chat-buddy-xuzf.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
